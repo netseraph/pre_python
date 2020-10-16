58553251b696944052618d107d534041f5c98367
netseraph <netseraph@gmail.com>
2020-10-16 10:50:24 +0800

    Update replace_source_ustc.sh

--- replace_source_ustc.sh
+++ replace_source_ustc.sh
@@ -1,5 +1,5 @@
 echo 将python的源更换为ustc
-pip3 config set extra-index-url https://mirrors.ustc.edu.cn/pypi/web/simple
+pip3 config set global.extra-index-url https://mirrors.ustc.edu.cn/pypi/web/simple
 pip3 config set global.index-url https://mirrors.ustc.edu.cn/pypi/web/simple
 pip3 config set install.trusted-host mirrors.ustc.edu.cn
 pip3 config list
