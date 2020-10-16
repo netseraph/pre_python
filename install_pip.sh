bb3c8a6dc0d91ecc957ab74f3c15648a7a68c8ba
netseraph <netseraph@gmail.com>
2020-10-16 11:41:50 +0800

    Update install_pip.sh

--- install_pip.sh
+++ install_pip.sh
@@ -1,5 +1,5 @@
 echo 更新pip
 sudo apt install python3-pip
-sudo apt-get install python-dev libxml2-dev libxslt1-dev zlib1g-dev
-python -m pip install --upgrade pip
+sudo apt-get install python3-dev libxml2-dev libxslt1-dev zlib1g-dev
+python3 -m pip install --upgrade pip
 
